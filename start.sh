f372f47d2445e7c92a1db27a9216625102e6a3ae
Jack Tubbenhauer <jack@tubbs.dev>
2026-03-16 16:28:54 +1100

    suppress noisy cloudflare bee ess

--- start.sh
+++ start.sh
@@ -23,7 +23,7 @@ trap cleanup INT TERM
 sleep 2
 if command -v cloudflared &> /dev/null; then
   echo "Starting cloudflared tunnel..."
-  cloudflared tunnel run dev-hub
+  cloudflared tunnel --loglevel warn run dev-hub
 elif command -v tailscale &> /dev/null; then
   echo "Starting tailscale funnel..."
   tailscale funnel --bg 3000
